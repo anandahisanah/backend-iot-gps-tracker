90fc94b599586d50f8376ac4a5b99b1168a43850
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-07-04 22:59:43 +0800

    feat: datatype time

--- models/chat.go
+++ models/chat.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Chat struct {
-	Id        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
-	Username  string    `gorm:"not null" json:"username"`
-	Message   string    `gorm:"not null" json:"message"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	Id        uint       `gorm:"primaryKey;autoIncrement" json:"id"`
+	Username  string     `gorm:"not null" json:"username"`
+	Message   string     `gorm:"not null" json:"message"`
+	CreatedAt *time.Time `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
 }
