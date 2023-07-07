4bc54b02f94e22950a4cc886ffd362cc9ca20343
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-07-07 11:46:21 +0800

    feat: config CORS

--- routes/route.go
+++ routes/route.go
@@ -4,6 +4,7 @@ import (
 	"backend-iot-gps-tracker/controllers"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,12 @@ var PORT = os.Getenv("PORT")
 
 func StartServer() *gin.Engine {
 	router := gin.Default()
+
+	// config CORS
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowMethods = []string{"GET", "POST"}
+	router.Use(cors.New(config))
 
 	router.GET("/", controllers.GetChat)
 	router.POST("/chat", controllers.CreateChat)
